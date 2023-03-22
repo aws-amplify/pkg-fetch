ffb20922b69a8e4d9ea7eb8ef4edb0bcbb9dc7b7
Zachary Goldberg <goldbez@amazon.com>
2023-03-22 14:04:26 -0700

    fix: fast fail if no fetched binaries

--- lib/index.ts
+++ lib/index.ts
@@ -135,7 +135,7 @@ export async function need(opts: NeedOptions) {
     }
   }
 
-  throw new Error('Could not find binaries in PKG_CACHE_PATH nor default USER/.pkg-cache. Amplify's fork requires that cache with binaries is prepared before executing this step.');
+  throw new Error("Could not find binaries in PKG_CACHE_PATH nor default USER/.pkg-cache. Amplify's fork requires that cache with binaries is prepared before executing this step.");
 
   if (!forceFetch) {
     if (await exists(built)) {
